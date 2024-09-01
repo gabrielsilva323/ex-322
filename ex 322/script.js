94591dd432c12a6859c5208f8fe9e415fe033639
Gabriel Silva <143452553+gabrielsilva323@users.noreply.github.com>
2024-09-01 12:37:28 -0300

    Update script.js

--- ex 322/script.js
+++ ex 322/script.js
@@ -5,4 +5,12 @@ const body = document.querySelector("body")
 let contador = 0
 
 while(contador < Games.leght) {
-  body.innerHTML += ``
+  body.innerHTML += `<div>
+        <h1>games</h1>
+        <h2>nome</h2>
+        <p>lançamento</p>
+        <p>tema</p>
+        <p>plataforma</p>
+        <p>iade para jogar</p>
+        <p>jamal joga?</p>
+     </div>`
